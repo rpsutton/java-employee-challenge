bd6e8bdcb518fe81de7e9b2c5b338e109c18d497
rpsutton <57606697+rpsutton@users.noreply.github.com>
2025-08-22 11:06:34 -0400

    adding employee model

---
+++ api/src/main/java/com/reliaquest/api/model/Employee.java
@@ -0,0 +1,30 @@
+package com.reliaquest.api.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private String id;
+
+    @JsonProperty("employee_name")
+    private String employeeName;
+
+    @JsonProperty("employee_salary")
+    private Integer employeeSalary;
+
+    @JsonProperty("employee_age")
+    private Integer employeeAge;
+
+    @JsonProperty("employee_title")
+    private String employeeTitle;
+
+    @JsonProperty("employee_email")
+    private String employeeEmail;
+}
